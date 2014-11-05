8f12e9a5ee2580e0bba19d1a68d94ec39190bf60
Jørgen Sirhaug <jsunborn@users.noreply.github.com>
2014-11-05 15:20:25 +0100

    Made "calculateTotalSum()"
    NB: This function, in addition to the helper function "loadXMLDoc()" is
    commented out, due to the inability to acces .xml-files on a local
    folder via JavaScript.

--- website/stylization/add_to_cart.js
+++ website/stylization/add_to_cart.js
@@ -4,6 +4,23 @@ var AddToCart = (function() {
 	var identifier = "WEBTEKBOARD:";
 	var stringsplit = ',';
 	
+	/*
+	function loadXMLDoc(filename) {
+		var xhttp;
+		if (window.XMLHttpRequest) {
+			xhttp=new XMLHttpRequest();
+		}
+		else { // IE5 and IE6
+			xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.open("GET",filename,false);
+		xhttp.send();
+		return xhttp.responseXML;
+	}
+	
+	var payment_info = loadXMLDoc("../product-info/payment_info.xml");
+	*/
+	
 	var getProductName = function() {
 		var metas = $tag("meta");
 		for(var i=0; i<metas.length; i++) {
@@ -13,6 +30,31 @@ var AddToCart = (function() {
 		}
 		return "";
 	}
+	
+	/*
+	var calculateTotalSum = function() {
+		var price = 0;
+		var products = payment_info.getElementsByTagName("name");
+		
+		// Loop over all products in storage
+		for(var i=0; i<sessionStorage.length; i++) {
+			// Get one product
+			var product = sessionStorage.getItem(sessionStorage.key(i)).split(stringsplit);
+			// Loop over all products in xml-file
+			for(var j=0; j<products.length; j++) {
+				// If (when) they match, get price and multiply by number of products
+				if(products[i].innerHTML == product[0]) {
+					console.log(product);
+					var prodPrice = products[i].parentNode.getElementsByTagName("price")[0].innerHTML;
+					console.log(prodPrice);
+					price += (prodPrice * product[1])
+					break;
+				}
+			}
+		}
+		console.log(price);
+	}
+	*/
 	
 	var addProduct = function() {
 		var product = getProductName();
@@ -28,6 +70,9 @@ var AddToCart = (function() {
 			}
 			sessionStorage.setItem((identifier+product[0]), product);
 		}
+		/*
+		calculateTotalSum();
+		*/
 	}
 	
 	return {
