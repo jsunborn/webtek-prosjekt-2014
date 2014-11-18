18f7c8cfa099a0ba855b375e245edb6e30707998
JØRGEN SIRHAUG <SIRHAUG751@GMAIL.COM>
2014-11-18 13:36:20 +0100

    Fixed featured .js and tilted 'Special Offer' text

--- website/stylization/frontpage-animation.js
+++ website/stylization/frontpage-animation.js
@@ -3,30 +3,35 @@ addEventListener('load', function() {
 
     if (canvas && canvas.getContext) {
         var ctx = canvas.getContext("2d");
-        var headerTextX = 10;
-        var headerTextY = 40;
-        var subTextX = 10;
-        var subTextY = 100;
+        var headerTextX = -20;
+        var headerTextY = 140;
+        var subTextX = -20;
+        var subTextY = 180;
         var color = 0;
 
         var headerText = "Special offer!";
         var subText = "Ticket to Ride - now only 365,-";
 
         var offerImage = new Image();
-        var offerImageX = 365;
+        var offerImageX = 450;
         var offerImageY = 20;
 
         offerImage.onload = function() {
+            
+            ctx.rotate(-(Math.PI * 7/100));
+
+            ctx.font='bold 50px Amaranth';
+            ctx.fillStyle = "#0000FF";
+            ctx.fillText(headerText, headerTextX, headerTextY);
+
+            ctx.font='bold 30px Voltaire';
+            ctx.fillStyle = "#000099";
+            ctx.fillText(subText, subTextX, subTextY);
+
+            ctx.rotate((Math.PI * 7/100));
+
             setInterval(function() {
-                ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-
-                ctx.font='bold 30px calibri';
-                ctx.fillStyle = "#0000FF";
-                ctx.fillText(headerText, headerTextX, headerTextY);
-
-                ctx.font='bold 20px calibri';
-                ctx.fillStyle = "#000099";
-                ctx.fillText(subText, subTextX, subTextY);
+                ctx.clearRect(380, 0, canvas.width, canvas.height); // Clear canvas
 
                 ctx.shadowBlur = 30;
                 ctx.shadowColor = "hsl(" + color % 360 + ",100%,40%)";
